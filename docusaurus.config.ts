1fce1ea4ee03941ecdee62714987ed93f38bed0c
Kodbex <kodbex.com@gmail.com>
2025-05-25 14:47:39 +0500

    Fix GitHub pages issue!

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
     favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
-    url: 'https://kodbex.github.com',
+    url: 'https://kodbex.github.io',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/baby-care-docs/',
