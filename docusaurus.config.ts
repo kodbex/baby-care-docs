89054189e2c60515e0fedcb9a99d04be30bcad1a
Kodbex <kodbex.com@gmail.com>
2025-05-25 18:36:12 +0500

    Created a design.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -43,21 +43,6 @@ const config: Config = {
                     editUrl:
                         'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
                 },
-                blog: {
-                    showReadingTime: true,
-                    feedOptions: {
-                        type: ['rss', 'atom'],
-                        xslt: true,
-                    },
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-                    // Useful options to enforce blogging best practices
-                    onInlineTags: 'warn',
-                    onInlineAuthors: 'warn',
-                    onUntruncatedBlogPosts: 'warn',
-                },
                 theme: {
                     customCss: './src/css/custom.css',
                 },
@@ -69,7 +54,7 @@ const config: Config = {
         // Replace with your project's social card
         image: 'img/docusaurus-social-card.jpg',
         navbar: {
-            title: 'My Site',
+            title: 'Baby Development Ui Kit',
             logo: {
                 alt: 'My Site Logo',
                 src: 'img/logo.svg',
@@ -78,10 +63,10 @@ const config: Config = {
                 {
                     type: 'docSidebar',
                     sidebarId: 'tutorialSidebar',
-                    position: 'left',
+                    position: 'right',
                     label: 'Tutorial',
                 },
-                {to: '/blog', label: 'Blog', position: 'left'},
+                // {to: '/blog', label: 'Blog', position: 'left'},
                 {
                     href: 'https://github.com/facebook/docusaurus',
                     label: 'GitHub',
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,36 +9,51 @@ import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className="container">
+                <Heading as="h1" className="hero__title">
+                    #1 Baby Development App Ui Kit
+                </Heading>
+                <p className="hero__subtitle">
+                    Baby Development App Ui Kit is developed and designed for Developer using React Native.
+                    Baby Development Kit Saves your 200+ hours work and quick start your project in apps like
+                </p>
+
+                {/* Banner Images*/}
+                <div>
+                    <img className="img-fluid mobile-screen" src="https://landing.pixelstrap.net/reactnative/multikit/assets/images/screens/mobile-screen.png" alt="mobile-screen"/>
+                </div>
+
+                <div className={styles.buttons}>
+                    <Link
+                        className="button button--secondary button--lg"
+                        to="/docs/intro">
+                        Docusaurus Tutorial - 5min ⏱️
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <Layout
+            title={`Hello from ${siteConfig.title}`}
+            description="Description will go into a meta tag in <head />">
+            <HomepageHeader/>
+            <main>
+                <HomepageFeatures/>
+            </main>
+            <main>
+                <HomepageFeatures/>
+            </main>
+            <main>
+                <HomepageFeatures/>
+            </main>
+        </Layout>
+    );
 }
